9f75431d38b88448ea66b37cbff9d15906e27ecd
Doug Schaefer <cdtdoug@gmail.com>
2025-04-18 22:28:09 -0400

    Add emacs keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,22 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("i", "<S-CR>", "<Esc>")
+
+-- Emacs keys
+vim.keymap.set("i", "<C-p>", "<Up>")
+vim.keymap.set("i", "<C-n>", "<Down>")
+vim.keymap.set("i", "<C-f>", "<Right>")
+vim.keymap.set("i", "<C-b>", "<Left>")
+vim.keymap.set("i", "<C-a>", "<Home>")
+vim.keymap.set("i", "<C-e>", "<End>")
+
+vim.keymap.set("n", "<C-v>", "<C-f>")
+vim.keymap.set("n", "<C-S-v>", "<C-b>")
+vim.keymap.set("n", "<C-p>", "<Up>")
+vim.keymap.set("n", "<C-n>", "<Down>")
+vim.keymap.set("n", "<C-f>", "<Right>")
+vim.keymap.set("n", "<C-b>", "<Left>")
+vim.keymap.set("n", "<C-a>", "<Home>")
+vim.keymap.set("n", "<C-e>", "<End>")

53e9e694cb95ab014b1d65064938073a31245f85
Doug Schaefer <cdtdoug@gmail.com>
2025-05-05 00:05:02 -0400

    verilog support

--- lua/plugins/verilog.lua
+++ lua/plugins/verilog.lua
@@ -5,9 +5,28 @@ return {
     },
     {
         "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.verible.setup({
+                cmd = { "verible-verilog-ls", "--rules_config_search" },
+                root_dir = lspconfig.util.root_pattern("verible.filelist", vim.fn.getcwd()),
+            })
+        end,
+    },
+    {
+        "stevearc/conform.nvim",
         opts = {
-            servers = {
-                verible = {},
+            formatters_by_ft = {
+                verilog = { "verible" },
+                systemverilog = { "verible" },
+            },
+            formatters = {
+                verible = {
+                    prepend_args = {
+                        "--indentation_spaces",
+                        "4",
+                    },
+                },
             },
         },
     },

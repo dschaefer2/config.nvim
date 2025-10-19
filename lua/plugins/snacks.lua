37968201189c3b5081ecb36a1779d8ddc6ebbc83
Doug Schaefer <cdtdoug@gmail.com>
2025-10-19 01:20:29 -0400

    Use regular file picker

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,7 +7,7 @@ return {
         },
         keys = {
             { "<leader>pb", function() require('snacks').picker.buffers() end, desc = "Pick buffers" },
-            { "<leader>pf", function() require('snacks').picker.smart() end, desc = "Pick files" },
+            { "<leader>pf", function() require('snacks').picker.files() end, desc = "Pick files" },
             { "<leader>pgs", function() require('snacks').picker.git_status() end, desc = "Git status" },
         }
     }

4f31b30cdd136973e8d327549ebaf872bde714c4
Doug Schaefer <cdtdoug@gmail.com>
2026-05-23 14:29:25 -0400

    Restart with mini

---
+++ init.lua
@@ -0,0 +1,11 @@
+require("vim._core.ui2").enable({})
+
+require("options")
+require("keymaps")
+require("commands")
+require("pack")
+require("treesitter")
+require("lsp")
+
+vim.cmd.colorscheme("catppuccin")
+

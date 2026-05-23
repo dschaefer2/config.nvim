510401fa3e54dca133498ad39c555839bb0f7875
Doug Schaefer <cdtdoug@gmail.com>
2026-05-23 14:49:14 -0400

    clean up keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,14 +8,12 @@ vim.keymap.set(mode, "<C-n>", "<Down>")
 vim.keymap.set(mode, "<C-p>", "<Up>")
 vim.keymap.set(mode, "<C-a>", "<Home>")
 vim.keymap.set(mode, "<C-e>", "<End>")
-vim.keymap.set(mode, "<C-d>", "<Del>")
+
+vim.keymap.set("n", "<C-g>", ":nohl<CR>", { desc = "Clear search highlighting", silent = true })
 
 vim.keymap.set("x", "p", [["_dP]], { desc = "Paste over selection without losing yanked text" })
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete without yanking" })
-
-vim.keymap.set("i", "<C-c>", "<Esc>")
-vim.keymap.set("n", "<C-c>", ":nohl<CR>", { desc = "Clear search highlighting", silent = true })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "moves lines down in visual selection" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "moves lines up in visual selection" })

93c9e19d5b7089657a3079e6844dcbd978e653c8
Doug Schaefer <dschaefer2@apple.com>
2026-06-10 13:31:42 -0400

    Add lsp and blink

--- lua/pack.lua
+++ lua/pack.lua
@@ -2,7 +2,9 @@ vim.pack.add({
     "https://github.com/stevearc/oil.nvim",
     "https://github.com/nvim-mini/mini.nvim",
     "https://github.com/nvim-tree/nvim-web-devicons",
-    "https://github.com/nvim-lualine/lualine.nvim"
+    "https://github.com/nvim-lualine/lualine.nvim",
+    "https://github.com/neovim/nvim-lspconfig",
+    "https://github.com/Saghen/blink.cmp",
 })
 
 -- oil
@@ -15,6 +17,34 @@ require("oil").setup({
 })
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- lsp
+
+vim.lsp.enable("sourcekit")
+
+-- blink
+
+require('blink.cmp').setup({
+    keymap = { preset = 'default' },  -- <C-y> to accept, <C-space> to open, <C-n>/<C-p> to cycle
+
+    appearance = {
+        nerd_font_variant = 'mono',
+    },
+
+    completion = {
+        documentation = { auto_show = true, auto_show_delay_ms = 200 },
+    },
+
+    sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
+
+    fuzzy = {
+        implementation = "lua",
+    },
+
+    signature = { enabled = true },
+})
 
 -- lualine
 

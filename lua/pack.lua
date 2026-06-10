61c93c00c5419da440a40395955492d5c4c0e1fd
Doug Schaefer <dschaefer2@apple.com>
2026-06-10 14:00:09 -0400

    fzf

--- lua/pack.lua
+++ lua/pack.lua
@@ -5,6 +5,7 @@ vim.pack.add({
     "https://github.com/nvim-lualine/lualine.nvim",
     "https://github.com/neovim/nvim-lspconfig",
     "https://github.com/Saghen/blink.cmp",
+    "https://github.com/ibhagwan/fzf-lua",
 })
 
 -- oil
@@ -44,6 +45,48 @@ require('blink.cmp').setup({
     },
 
     signature = { enabled = true },
+})
+
+-- fzf-lua
+
+require("fzf-lua").setup({
+    'default',
+})
+
+local fzf = require('fzf-lua')
+vim.keymap.set('n', '<leader>ff', fzf.files,        { desc = 'Find files' })
+vim.keymap.set('n', '<leader>fg', fzf.live_grep,    { desc = 'Live grep' })
+vim.keymap.set('n', '<leader>fb', fzf.buffers,      { desc = 'Buffers' })
+vim.keymap.set('n', '<leader>fh', fzf.helptags,     { desc = 'Help tags' })
+vim.keymap.set('n', '<leader>fr', fzf.resume,       { desc = 'Resume last picker' })
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+        local opts = { buffer = args.buf }
+        local fzf = require('fzf-lua')
+
+        -- navigation
+        vim.keymap.set('n', 'gd', fzf.lsp_definitions,      opts)
+        vim.keymap.set('n', 'gD', fzf.lsp_declarations,     opts)
+        vim.keymap.set('n', 'gi', fzf.lsp_implementations,  opts)
+        vim.keymap.set('n', 'gr', fzf.lsp_references,       opts)
+        vim.keymap.set('n', 'gt', fzf.lsp_typedefs,         opts)
+
+        -- symbols
+        vim.keymap.set('n', '<leader>ds', fzf.lsp_document_symbols,           opts)
+        vim.keymap.set('n', '<leader>ws', fzf.lsp_live_workspace_symbols,     opts)
+
+        -- diagnostics
+        vim.keymap.set('n', '<leader>dd', fzf.lsp_document_diagnostics,       opts)
+        vim.keymap.set('n', '<leader>dw', fzf.lsp_workspace_diagnostics,      opts)
+
+        -- actions (fzf-lua provides a nice picker UI for these too)
+        vim.keymap.set('n', '<leader>ca', fzf.lsp_code_actions,              opts)
+
+        -- these aren't fzf pickers, keep the native ones
+        vim.keymap.set('n', 'K',  vim.lsp.buf.hover,  opts)
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    end,
 })
 
 -- lualine
